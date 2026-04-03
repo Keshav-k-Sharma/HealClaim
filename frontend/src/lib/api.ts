527101f999057bc73396aaa66aa28654da87577c
keshav krishan sharma <reach.keshavks@gmail.com>
2026-04-03 15:53:42 +0530

    adding backend url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -25,7 +25,7 @@ import type {
 
 // ─── Axios instance ───────────────────────────────────────────────────────────
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080/api"
+const BASE_URL ="https://w9kabl1z9b.execute-api.ap-south-1.amazonaws.com/Prod/api" 
 
 const api: AxiosInstance = axios.create({
   baseURL: BASE_URL,
